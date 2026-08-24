2d0d0675f3be4bfb16c14c9f49eeefe46f2533fa
bestdealshoping <jeromjp35@gmail.com>
2026-08-24 07:07:04 -0400

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -3,8 +3,15 @@ const SUPABASE_URL = "https://jmkwekgpsogbdpkjqqyx.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_b8t3rG-3ErqKmrhdTT0qYA_IOFHFLKP";
 
-window.supabaseClient =
-  window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_KEY
-  );
+if (!window.supabase) {
+  console.error("❌ Supabase SDK pa chaje.");
+  throw new Error("Supabase SDK introuvable.");
+}
+
+window.supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
+
+console.log("✅ Supabase Client OK");
+
