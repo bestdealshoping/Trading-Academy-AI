cc85ff87ff28808ef89d2d20ea70efebebefacb7
bestdealshoping <jeromjp35@gmail.com>
2026-08-24 01:50:48 -0400

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,11 +1,10 @@
-const SUPABASE_URL =
-    "https://jmkwekgpsogbdpkjqqyx.supabase.co";
+const SUPABASE_URL = "https://jmkwekgpsogbdpkjqqyx.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    "sb_publishable_b8t3rG-3ErqKmrhdTT0qYA_IOFHFLKP";
+  "sb_publishable_b8t3rG-3ErqKmrhdTT0qYA_IOFHFLKP";
 
-const supabaseClient =
-    window.supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_PUBLISHABLE_KEY
-    );
+window.supabaseClient =
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+  );
